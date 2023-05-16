2d5ecdb1545d9f2c1259b337d8adeebf31ffd860
mohammed <mohammedshawwa@mohammeds-MacBook-Pro-2.local>
2023-05-16 14:07:13 +0300

    add question page

---
+++ my_app/lib/AddNewQuestion.dart
@@ -0,0 +1,33 @@
+
+
+
+
+// class addNewQuestion extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Color.fromARGB(255, 0, 121, 109),
+//         title: Text('Add New Question'),
+//       ),
+//       body: Container(
+        
+//       ),
+//     );
+//   }
+// }
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AddNewQuestion extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+appBar: AppBar(
+backgroundColor: Color.fromARGB(255, 0, 121, 109),
+title: Text('Add New Question'),
+),
+body: Container(),
+);
+}
+}
